35b8ca1c604a535f9a1d9646ae5608c957bf6dcf
Александр Важнёв <vazhnevalex@gmail.com>
2025-02-18 19:37:32 +0300

    Добавьте файлы проекта.

---
+++ Task/Task.cpp
@@ -0,0 +1,15 @@
+﻿/*Прогрмма по нахождению площади потём ввода периметра*/
+#include <iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int s;
+    int p;
+    cin >> p;
+    s = pow(
+        p / 4, 2);
+    cout << s << endl;
+    return 0;
+}
